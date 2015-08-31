6cc453bfd9adaffdd04900e2d46f15049f6682a3
Corey Powell <mistdragon100@gmail.com>
2015-08-31 09:04:57 -0500

    Bumped version to 0.13.0

--- lib/minil/version.rb
+++ lib/minil/version.rb
@@ -1,6 +1,6 @@
 module Minil
   module Version
-    MAJOR, MINOR, TEENY, PATCH = 0, 12, 0, nil
+    MAJOR, MINOR, TEENY, PATCH = 0, 13, 0, nil
     STRING = [MAJOR, MINOR, TEENY, PATCH].compact.join('.')
   end
   VERSION = Version::STRING
